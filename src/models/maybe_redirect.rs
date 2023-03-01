a252741097552541bbcf9322c03a67224818828a
Leonora Tindall <nora@nora.codes>
2023-03-01 15:12:52 -0600

    Fix various build warnings.

--- src/models/maybe_redirect.rs
+++ src/models/maybe_redirect.rs
@@ -1,6 +1,9 @@
 use rocket::{response::Redirect, Responder};
 use rocket_dyn_templates::Template;
 
+// Allowed LEV because Rocket doesn't let us box Responders like Redirect.
+// See https://github.com/SergioBenitez/Rocket/issues/568
+#[allow(clippy::large_enum_variant)]
 #[derive(Responder)]
 pub enum MaybeRedirect {
     Redirect(Redirect),
